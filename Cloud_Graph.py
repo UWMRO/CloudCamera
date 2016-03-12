b484b4850fd988230a8645177f6782a31e8d7d02
Ssoyrnoz <jmarmstr@uw.edu>
2016-03-11 20:38:13 -0800

    Matt - Error handling and improved output

--- Cloud_Graph.py
+++ Cloud_Graph.py
@@ -1,14 +1,14 @@
-'''
+"""
 Test code for Cloud Camera histogram plotting
 Used to determine the mean value of each row in an image
 to be used for later analysis
-'''
+"""
 
-__author__ = "J. Matt Armstrong"
+__author__ = ["J. Matt Armstrong","Nathen Nguyen"]
 __copyright__ = "NA"
-__credits__ = ["Nathen Nguyen", "Joseph Huehnerhoff"]
+__credits__ = ["Joseph Huehnerhoff"]
 __license__ = "GPL"
-__version__ = "0.3"
+__version__ = "0.6"
 __maintainer__ = "J. Matt Armstrong"
 __email__ = "jmarmstr@uw.edu"
 __status__ = "Developement"
@@ -28,6 +28,7 @@ import os
 from PIL import Image
 import itertools
 import sys
+from subprocess import call
 
 
 
@@ -37,52 +38,56 @@ class CloudGraph(object):
 		self.dir = os.path.join(os.getcwd(),'logs')
 		self.logType = 'cloud' # Parameter used in  Logger class to create logfile
 
-	'''
-	These were functions used for testing how to mask the image, saved for
-	future use.
+	def start_up_checks(self):
+		"""
+		Run once at start-up
+		Checks for necessary folders and files
+		Creates them if necessary
+		"""
 
-	def mask(self, pixel_list):
-		median = np.median(pixel_list)
-		std = np.std(pixel_list)
-		#print median
-		result = pixel_list
+		if os.path.isfile("static_mask.npy") == True:
+			print 'Loading static mask file.'
+		else:
+			print "Static mask file not found, making one now."
+			print self.make_static_mask(500)
+		static_mask = np.load("static_mask.npy")
 
-		for i in range(len(pixel_list)):
-			if pixel_list[i] > (2 * median):
-				result[i] = 0
-			elif pixel_list[i] < (median / 2):
-				result[i] = 0
-			else:
-				result[i] = pixel_list[i]
-		return result
+		folder_list = ["logs", "images", "analyzed"]
+		for folder in folder_list:
+			if (os.path.isdir(folder)) == False:
+				print "Creating directory "+str(folder)
+				os.makedirs(folder)
 
-	def masked_img(self, image, mask_val):
-		result = []
-		median = np.median(image)
-		std = np.std(image)
-		print std
-		for row in range(len(image)):
-			temp_row = []
-			for pixel in image[row]:
-				if pixel > ((mask_val) * std):
-					temp_row.append(0)
-				elif pixel < (std / (mask_val)):
-					temp_row.append(0)
-				else:
-					temp_row.append(pixel)
-			result.append(temp_row)
-		return result
-	'''
+		listdirect = os.path.join(os.getcwd(),'images/')
+		imagelist = listdirect+'image.txt'
+
+		if os.path.isfile(str(imagelist)) == True:
+			print "Found and loading image list"
+		else:
+			print "Could not find image list (images/image.txt)"
+			print "Creating a list of fits files in images/"
+			fitslist = []
+			for fits in os.listdir(listdirect):
+				if fits.endswith(".fits"):
+					fitslist.append(fits)
+			f = open(imagelist, "w")
+			f.write("\n".join(map(lambda x: str(x), fitslist)))
+			f.close()
+
+		img_list = np.genfromtxt(imagelist, usecols = [0], unpack = True, dtype = 'str')
+
+		return img_list, static_mask
+
 
 	def make_static_mask(self, radius):
-		'''
+		"""
 		Used to make a static aperture mask, which can be
 		multiplied by the analysis image to remove any pixels
 		outside the radius from the center.
 
 		Input: Radius to mask from center of image
 		Output: Aperture asked numpy array the same size as the image.
-		'''
+		"""
 		result = []
 
 		for x in range(1024):
@@ -99,13 +104,13 @@ class CloudGraph(object):
 		return "Static mask saved"
 
 	def dynamic_mask(self, image, sigrange):
-		'''
+		"""
 		Creates a numpy mask on the image, filtering out any
 		pixel values that are more than sigrange*std from the median value
 
 		Input: numpy array of the image, sigrange for multiplier on standard dev range
 		Output: Masked numpy array covering any pixels above or below the standard dev range
-		'''
+		"""
 
 		# Make a masked array using the static mask and imput image
 		pre_masked = ma.array(image, mask=static_mask)
@@ -124,13 +129,13 @@ class CloudGraph(object):
 
 
 	def pixel_value_list(self, image):
-		'''
+		"""
 		Produce two arrays, the first is a list of the binned pixel values
 		and the second is the number of pixels at that value
 
 		Input: masked numpy array of the image
 		Output: Two lists of histogram data on the image. ([bins],[values])
-		'''
+		"""
 
 		# Compress the array into a list of pixel values
 		compressed = image.compressed()
@@ -143,12 +148,12 @@ class CloudGraph(object):
 		return result
 
 	def plot_histogram(self, values, bins, img_out, masked, median, mean, std, name):
-		'''
+		"""
 		Statistical plotting and output function.
 
 		Input: Value list, Bin list, Name of output, Masked image, median value, mean value, standard dev, image name
 		Output: png image file with the masked image, statistical and image information, and histogram plot
-		'''
+		"""
 
 		plt.clf()
 
@@ -191,9 +196,10 @@ class CloudGraph(object):
 		ax1.xaxis.label.set_color('white')
 		ax1.yaxis.set_label_position("right")
 		ax1.yaxis.tick_right()
-		plt.yticks(np.arange(0,(np.max(values*100))))
-		ax1.tick_params(axis='x', colors='white', labelsize=16)
-		ax1.tick_params(axis='y', colors='white', labelsize=16)
+		plt.locator_params(axis='y',nbins=6)
+		ax1.tick_params(axis='x', colors='white', labelsize=12)
+		ax1.tick_params(axis='y', colors='white', labelsize=12)
+
 		plt.draw()
 
 		#Save the figure as a png
@@ -203,7 +209,7 @@ class CloudGraph(object):
 		return
 
 	def fits_to_list(self, file_name):
-		'''
+		"""
 		Try to open the fits file.
 		If the file doesn't exist, say so and return.
 		Otherwise, select just the image data as a numpy array
@@ -211,18 +217,18 @@ class CloudGraph(object):
 
 		Input: File name
 		Output: Numpy array of image data
-		'''
+		"""
 		hdulist = fits.open(file_name)
 		return np.asarray(hdulist[0].data)
 		hdulist.close()
 
 	def run_analysis(self, img_in, img_out, name):
-		'''
+		"""
 		This is where the code is actually run, so the total analysis
 		package can be called from outside this file.
 
 		Input: name of image in, name of image out, and chopped file name
-		'''
+		"""
 		img = self.fits_to_list(str(img_in))
 		print "Analyzing "+str(img_in)
 
@@ -244,7 +250,7 @@ class CloudGraph(object):
 
 
 if __name__=="__main__":
-	'''
+	"""
 	Run terminal command in image directory(ls *.fits > image.txt)
 	This will produce a text list of all fits files to process
 	Run the program using a list of file names in a text file_path
@@ -252,33 +258,20 @@ if __name__=="__main__":
 	Change the directory for images and create a folder inside
 	/analyzed
 
-	Log file will go to a /logs folder from this program's directory
-	img_name, median, mean, std
-	'''
+	Log file will contain time stamp, img_name, median, mean, std
+	"""
+
+	location = os.getcwd()
+	imagedir = os.path.join(location,'images/')
+
 
 	cg = CloudGraph()
-	dir = '/home/matt/College/AUEG/CloudCamera-master/Images/'
-	list = dir+'image.txt'
+	img_list, static_mask = cg.start_up_checks()
 
-	if os.path.isfile("static_mask.npy") == True:
-		print 'Loading static mask file.'
-		static_mask = np.load("static_mask.npy")
-	else:
-		print "Static mask file not found, making one now."
-		print cg.make_static_mask(500)
-		static_mask = np.load("static_mask.npy")
-
-	'''
-	test_img = "20160221T205857_30"
-	print cg.run_analysis(test_img+".fits", test_img+'_analyzed.png', test_img)
-
-	'''
-	img_list = np.genfromtxt(list, usecols = [0], unpack = True, dtype = 'str')
 
 	for i in img_list:
 		name = i.replace(".fits","")
-		if os.path.isfile(dir+name+".fits") == True:
-			name = name.replace("bias_","")
-			print cg.run_analysis(dir+name+".fits", dir+"analyzed/"+name+'_analyzed.png', name)
+		if os.path.isfile(imagedir+name+".fits") == True:
+			print cg.run_analysis(imagedir+name+".fits", location+"/analyzed/"+name+'_analyzed.png', name)
 		else:
 			print "File not found"
