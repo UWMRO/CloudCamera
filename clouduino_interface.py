88a337a8371cf8103713ac73d645992575599d08
Ssoyrnoz <jmarmstr@uw.edu>
2016-07-11 13:33:56 -0600

    Update clouduino_interface.py

--- clouduino_interface.py
+++ clouduino_interface.py
@@ -1,115 +1,73 @@
-#! /usr/bin/python
-
-"""
-clouduino_interface.py
-
-This program is designed to read in data from an arduino.  
-Specifically this is program interfaces to the arduino on the
-cloud camera.  Functionality includes reading the temperature,
- humidity, and light levels.
-
-TODO:
-finish interface
-
-Usage:
-
-Options:
+#include <Wire.h>
+#include <OneWire.h>
+#include <Servo.h>
 
 
-"""
+int servoPin = 9;       // light servo connected to pin 9
+int heatPin = 13;      // Heater connected to digital pin 13
+int dsPin = 10;
+int pos = 0;
+int inPos = 80;
+int outPos = 42;
+int servoDelay = 10;
 
-__author__ = ["Joseph Huehnerhoff", "Matt Armstrong", "Andrew Wilkins"]
-__license__ = "GPL"
-__version__ = "0.1"
-__maintainer__ = "Matt Armstrong"
-__email__ = ""
-__status__ = "Developement"
+#define ONE_WIRE_BUS dsPin
+OneWire oneWire(ONE_WIRE_BUS);
+OneWire  ds(dsPin);
 
-import serial
-import time
+Servo myservo;
 
-class ClouduinoInterface():
-    def __init__(self):
-	self.ser = None
-	self.serPort = '/dev/ttyACM0'
 
-    def log(self):
-    	"""
-    	Check the serial port for data
-    	and write any data with a timestamp
-    	to the savefile
-    	"""
-    	data = ser.readline()
-    	f = open(savefile, 'a')
-    	f.write(str(time.strftime("%H%M%S"))+","+str(data))
-    	f.close()
+void setup(void) {
+  Serial.begin(9600);
+  //myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+  Serial.println("past here");
 
-    def openPort(self):
-	""" open the serial port for communication with the arduino"""
-        self.ser=serial.Serial(self.serPort, 9600)
-        return
+}
 
-    def closePort(self):
-	""" close the serial port connection to the arduino"""
-        self.ser.close()
-        return
+void setFilterIn(){
+      if (pos != inPos){
+        myservo.attach(servoPin);     // attach to the servo
+        delay(1000);      // wait for the servo to attach
+        myservo.write(inPos);              // tell servo to go to position in variable 'pos'
+        delay(servoDelay);                       // waits 15ms for the servo to reach the position
+        }  
+        delay(1500);
+        pos = inPos;
+    myservo.detach();     // detach from the servo
+    return;
+}
 
-    def readSer(self):
-	""" Read in the arduino output, parse, and return something useful
-	Arguments:
-		None
-	Returns:
-		s (string): parsed serial output
-	"""
-        s = self.ser.readline()
-	print s
-        return s
+void setFilterOut(){
+   if (pos != outPos){
+      myservo.attach(servoPin);     // attach to the servo
+      delay(1000);      // wait for the servo to attach
+      myservo.write(outPos);              // tell servo to go to position in variable 'pos'
+      delay(servoDelay);                       // waits 15ms for the servo to reach the position
+      }
+      delay(1500);
+      pos = outPos;
+   myservo.detach();       // detach from the servo
+   return;
+ }
 
-    def getLux(self):
-	""" calls the arduino getLux() function
-	Arguments:
-		None
-	Returns:
-		flux (float): flux level from light sensor
-	"""
-	self.ser.write('l')
-	flux = float(self.readSer())  # this needs some error checking, I could see it causing problems later
-	print flux
-	return flux
+void test(void){
+  Serial.println("test routine");
+}
 
-    def setFilterPos(self, pos):
-	""" pos accepts True or False.  If true then it moves into position
-	Arguments:
-		pos (bool): True if filter should be in path, False if not
-	Retruns:
-		None
-	"""
-	if pos:
-		self.ser.write('i')
-	else:
-		self.ser.write('o')
-	return
-	
-    def getDomeStatus(self):
-	"""returns the dome metrology in format humidity,temp,pos
-	Arguments:
-		None
-	Returns:
-		
-
-	"""
-	self.ser.write('d')
-	time.sleep(1)
-	line = self.ser.readline()
-	l = line.split()
-	print l
-	return l
-
-if __name__ == "__main__":
-	c = ClouduinoInterface()
-	c.openPort()
-	time.sleep(2)
-	c.setFilterPos(False)
-	#print c.getLux()
-	time.sleep(1)
-	c.closePort()
+void loop(void) {
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    switch (inByte) {
+      case 'i':
+        setFilterIn();
+        break;
+      case 'o':
+        setFilterOut();
+        break;
+      case 'y':
+         test();
+         break;
+        }
+    }
+}
