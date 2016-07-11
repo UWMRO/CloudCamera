9c4113c61719d9bea04e59445b606a83fe655829
Ssoyrnoz <jmarmstr@uw.edu>
2016-07-11 15:34:12 -0700

    turned binary erosion back on for testing

--- Cloud_Graph.py
+++ Cloud_Graph.py
@@ -288,17 +288,17 @@ class CloudGraph(object):
 
 		plt.clf()
 
-		#Fill in the masked image for processing
-		masked_img = masked.filled(fill_value = 0)
-
 		#Use binary erosion to smooth the image
-		fixed_img = scipy.ndimage.morphology.binary_erosion(masked_img)
+		fixed_img = scipy.ndimage.morphology.binary_erosion(masked)
+
+		#Fill in the masked image for processing
+		masked_img = fixed_img.filled(fill_value = 0)
 
 		if self.scaleimg == True:
-			scaled_img = self.scale_img(fixed_img, median, std)
+			scaled_img = self.scale_img(masked_img, median, std)
 			img = Image.fromarray(scaled_img)
 		else:
-			img = Image.fromarray(fixed_img)
+			img = Image.fromarray(masked_img)
 
 		#Set up plotting environment
 		fig, ax = plt.subplots(2,2)
