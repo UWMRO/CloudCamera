af22abf42f5ae74d4f832c8f0eb4d6192c52e63f
Ssoyrnoz <jmarmstr@uw.edu>
2016-07-11 13:28:26 -0700

    removed binary erosion

--- Cloud_Graph.py
+++ Cloud_Graph.py
@@ -289,13 +289,13 @@ class CloudGraph(object):
 		plt.clf()
 
 		masked_img = masked.filled(fill_value = 0)
-		fixed_img = scipy.ndimage.morphology.binary_erosion(masked_img)
+		#fixed_img = scipy.ndimage.morphology.binary_erosion(masked_img)
 		#Fill in the masked image for processing
 		if self.scaleimg == True:
-			scaled_img = self.scale_img(fixed_img, median, std)
+			scaled_img = self.scale_img(masked_img, median, std)
 			img = Image.fromarray(scaled_img)
 		else:
-			img = Image.fromarray(fixed_img)
+			img = Image.fromarray(masked_img)
 
 		#Set up plotting environment
 		fig, ax = plt.subplots(2,2)
