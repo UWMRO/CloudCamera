227148288046c7a88cade4e83bae188f8cc8fe1e
Ssoyrnoz <jmarmstr@uw.edu>
2016-07-18 15:36:26 -0600

    jwh rebuild

--- Cloud_gif.py
+++ Cloud_gif.py
@@ -16,50 +16,45 @@ class CloudGif(object):
 		self.trans = transfer()
 
 	def findImg(self, dir = None):
-	       	listdirect = os.path.join(os.getcwd(),'gif/')
-	        imagelist = listdirect+'giflist.txt'
-
-        	print "Creating a list of png files in gif/"
+	       	listdirect = os.path.join(os.getcwd(),dir)
+	        
+        	print "Creating a list of png files in "+str(dir)
         	imglist = []
         	for img in os.listdir(listdirect):
                 	if img.endswith(".png"):
-                        	img = "gif/"+img
+                        	img = dir+"/"+img
                         	imglist.append(img)
         	if len(imglist) == 0:
-                	print "No images found in gif/"
+                	print "No images found in "+str(dir)
                 	print "Ending Cloud_gif.py"
 			return
-        	f = open(imagelist, "w")
-        	f.write("\n".join(map(lambda x: str(x), imglist)))
-        	f.close()
+        	
+        	#img_list = np.genfromtxt(imagelist, usecols = [0], unpack = True, dtype = 'str')
+        	print "gif list has "+str(len(imglist))+" entries"
+		return imglist
 
-        	img_list = np.genfromtxt(imagelist, usecols = [0], unpack = True, dtype = 'str')
-        	print "gif list has "+str(len(img_list))+" entries"
-		return
-
-	def make_gif(self, killmax, imglist):
+	def make_gif(self, killmax, imglist, dir=None):
 		#produce a gif of the last 10 images when self.count == 10
 		print "Producing gif image"
 		killcount = 0
 		if os.path.isfile('latest.gif'):
 			os.remove("latest.gif")
-		'''
-		command = "convert -delay 40 -loop 0 "+os.getcwd()+"/gif/*.png latest.gif"
+		
+		command = "convert -delay 40 -loop 0 "+os.getcwd()+"/"+dir+"/*.png latest.gif"
 		out = subprocess.Popen(command, stdout = subprocess.PIPE, shell=True)
 		stdoutp, stderrp = out.communicate()
-		while stdoutp == None:
+		while os.path.isfile('latest.gif') == False:
 			print "subprocess output:"+str(stdoutp)
 			if killcount == killmax:
-				stdoutp = "ran out of time"
+				print "ran out of time"
 			else:
 				print "gif not ready"
 				print str(killcount)
 				killcount += 1
 				time.sleep(1)
-		'''
-
-		images = [Image.open(fn) for fn in imglist]
-		writeGif('latest.gif', images, duration=0.5, subRectangles=False)
+		
+		#images = [Image.open(fn) for fn in imglist]
+		#writeGif('latest.gif', images, duration=0.5, subRectangles=False)
 
 		#print "sleeping while gif is produced"
 		#time.sleep(120)
@@ -91,7 +86,7 @@ if __name__ ==  "__main__":
 	cg = CloudGif()
 	
 	while True:
-		l = cg.findImg('gif')
+		l = cg.findImg('giftest')
 		imgList = sorted(l)
-		cg.make_gif(180, imgList)
+		cg.make_gif(180, imgList[:20], 'giftest')
 
