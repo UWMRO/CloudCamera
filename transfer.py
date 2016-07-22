132e08b0ec0d2c5243fc42d096a917db64707f8b
Joseph Huehnerhoff <jwhueh@gmail.com>
2016-07-22 12:49:34 -0600

    updated transfer

--- transfer.py
+++ transfer.py
@@ -25,46 +25,59 @@ import sys
 
 class transfer(object):
 	def __init__(self):
-		self.parm = self.retrieveParm()
-		self.ssh = "galileo.apo.nmsu.edu"
+		self.ssh = None
 		self.ftp = None
 
 	def openConnection(self, server = None, user = None):
 		self.ssh = paramiko.SSHClient()
 		keyfile = os.path.expanduser('~/.ssh/id_rsa')
 		password = keyring.get_password('SSH', keyfile)
-		key = paramiko.RSAKey.from_private_key_file(keyfile, password='mro2015')
+		key = paramiko.RSAKey.from_private_key_file(keyfile, password=password)
 		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		self.ssh.connect(self.parm['server'], username=self.parm['user'], pkey = key, timeout = 30)
+		self.ssh.connect(server, username=user, pkey = key, timeout = 30)
 		self.ftp = self.ssh.open_sftp()
+		return
 
 	def transProgress(self, trans, toBeTrans):
 		sys.stdout.write("\rtransferred: {0:.0f} %".format((trans / toBeTrans) * 100))
 		sys.stdout.flush()
 
 
-	def uploadFile(self, f_in):
-		print("uploading: ", f_in)
-		self.ftp.put(f_in,os.path.join(self.parm['server_dir'], f_in), callback=self.transProgress)
-		print("")
+	def uploadFile(self, server = None, user = None, f_in = None, server_dir = None):
+		print(time.strftime("%Y%m%dT%H%M%S  Uploading:  "), f_in)
+		self.openConnection(server, user)
+		self.ftp.put(f_in,os.path.join(server_dir, f_in), callback=self.transProgress)
+		self.closeConnection()
+		print(time.strftime("\n%Y%m%dT%H%M%S  Connection Closed"))
 		return
+
+	def downloadFile(self, server = None, user = None, server_file = None, local_file = None):
+		print(time.strftime("%Y%m%dT%H%M%S  Uploading:  "), server_file)
+                self.openConnection(server, user)
+                self.ftp.get(server_file, local_file, callback=self.transProgress)
+                self.closeConnection()
+                print(time.strftime("\n%Y%m%dT%H%M%S  Connection Closed"))
+                return
+
+
+	def findFiles(self, server = None, user = None, server_dir = None):
+		print(time.strftime("%Y%m%dT%H%M%S  Looking in:  "), server_dir)
+                self.openConnection(server, user)
+		files = self.ftp.listdir(server_dir)
+		self.closeConnection()
+                print(time.strftime("%Y%m%dT%H%M%S  Connection Closed"))
+                return files
 
 	def closeConnection(self):
 		self.ftp.close()
 		self.ssh.close()
 		return
 
-	def retrieveParm(self, f_loc = None):
-		dict = {}
-		f_in = open(os.path.join(os.getcwd(), 'transfer.init'),'r')
-		for line in f_in:
-			l = line.split()
-			dict[l[0]] = l[1]
-		f_in.close()
-		return dict
-
 if __name__ == "__main__":
 	t = transfer()
-	t.openConnection()
-	t.uploadFile('/var/www/html/latest.png')
-	t.closeConnection()
+	#t.openConnection('galileo.apo.nmsu.edu','jwhueh')
+	#t.closeConnection()
+	#t.uploadFile('galileo.apo.nmsu.edu', 'jwhueh', 'test.png', 'public_html/CloudCamera/')
+	#t.downloadFile('galileo.apo.nmsu.edu', 'jwhueh', 'public_html/CloudCamera/test.png', 'test.png')
+	print (t.findFiles('galileo.apo.nmsu.edu', 'jwhueh', 'public_html/CloudCamera/'))
+
